e9665b389b7e3259d1dfaafbea2a8f7559c1e2fb
Errol W <ewatsonrei@gmail.com>
2018-11-27 18:28:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,40 @@ console.log("Height:")
 console.log(height+1)
 
 var a = 5, b = 2, c = 3, d = "hello", e = "goodbye"
+
+function doNothing() {}
+
+doNothing()
+
+function sayHello() {
+  console.log('Hello!')
+}
+
+sayHello()
+
+function sayHelloToIsabel() {
+  console.log('Hello, Isabel!')
+}
+
+function sayHelloToSofia() {
+  console.log('Hello, Sofia!')
+}
+
+function sayHelloToBrendan() {
+  console.log('Hello, Brendan!')
+}
+
+sayHelloToIsabel()
+sayHelloToSofia()
+sayHelloToBrendan()
+
+function doSomething(thing) {
+  console.log(thing)
+}
+
+doSomething('anything')
+
+
+
+
+

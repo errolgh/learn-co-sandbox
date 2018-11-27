284d21ddf92a963716e5e4387e18338d41838637
Errol W <ewatsonrei@gmail.com>
2018-11-27 19:59:52 +0000

    Done.

--- index.js
+++ index.js
@@ -10,3 +10,65 @@ console.log("Height:")
 console.log(height+1)
 
 var a = 5, b = 2, c = 3, d = "hello", e = "goodbye"
+
+function doNothing() {}
+
+doNothing()
+
+function sayHello() {
+  console.log('Hello!')
+}
+
+sayHello()
+
+function sayHelloToIsabel() {
+  console.log('Hello, Isabel!')
+}
+
+function sayHelloToSofia() {
+  console.log('Hello, Sofia!')
+}
+
+function sayHelloToBrendan() {
+  console.log('Hello, Brendan!')
+}
+
+sayHelloToIsabel()
+sayHelloToSofia()
+sayHelloToBrendan()
+
+function doSomething(thing) {
+  console.log(thing)
+}
+
+doSomething('anything')
+
+function sayHelloTo(firstName) {
+  console.log(`Hello, ${firstName}!`)
+}
+
+sayHelloTo('Isabel') // "Hello, Isabel!"
+sayHelloTo('Jane') // "Hello, Jane!"
+sayHelloTo('R2-D2') // "Hello, R2-D2!"
+sayHelloTo(1) // "Hello, 1!"
+
+function say(greeting, firstName) {
+  console.log(`${greeting}, ${firstName}!`)
+}
+
+say("Goodbye", "Julio")
+
+say("Julio", "Goodbye")
+
+// Wrong command in lesson below?
+
+function add(x, y) {
+  console.log(x + y)
+}
+
+add(1, 2)
+
+
+
+
+

f981a6b54e560f1e365095859dd9c2257b6e6cea
Errol W <ewatsonrei@gmail.com>
2018-11-27 19:25:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -52,6 +52,10 @@ sayHelloTo('Jane') // "Hello, Jane!"
 sayHelloTo('R2-D2') // "Hello, R2-D2!"
 sayHelloTo(1) // "Hello, 1!"
 
+function say(greeting, firstName) {
+  console.log(`${greeting}, ${firstName}!`)
+}
 
+say("Goodbye", "Julio")
 
 
